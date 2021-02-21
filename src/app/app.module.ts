d525c60668621c8611b24fa70dec896bf69752c7
Mayur Palase <mayurpalase01@gmail.com>
2021-02-21 09:51:47 +0530

    Angular-Jump about, customers, footer, header, login and order components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { OrderComponent } from './components/order/order.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CustomersComponent } from './components/customers/customers.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    OrderComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    CustomersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+      $('input').on('focusin', function() {
+        $(this).parent().find('label').addClass('active');
+      });
+
+      /* $('input').on('focusout', function() {
+          if (!this.value) {
+            $(this).parent().find('label').removeClass('active');
+          }
+      }); */
+  }
+}
